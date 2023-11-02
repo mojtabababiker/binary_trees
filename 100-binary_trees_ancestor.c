02747bdab50413f173c853ebfdc002bde85d9145
mojtabababiker <mojmohammad98@gmail.com>
2023-11-03 00:50:42 +0200

    Edit task 100, inhanced the recursive calll logic

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -11,6 +11,8 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second)
 {
+	binary_tree_t *c_a = NULL;
+
 	if (first == NULL || second == NULL)
 		return (NULL);
 
