2b9e537ad4023e9a12505f82aa2b59c1a612f2d9
mojtabababiker <mojmohammad98@gmail.com>
2023-11-06 15:15:49 +0200

    Edit task 110

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,5 +1,8 @@
 #include "binary_trees.h"
 
+int check_left(binary_tree_t *tree, int root_val);
+int check_right(binary_tree_t *tree, int root_val);
+
 /**
  * binary_tree_is_bst - check if the tree is binary search tree
  * @tree: a binary tree to check
@@ -19,18 +22,84 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 	l_node = tree->left;
 	r_node = tree->right;
 
+	return (check_left(l_node, tree->n) * check_right(r_node, tree->n));
+}
+
+/**
+ * check_left - check the left sub-tree
+ * @tree: the left sub-tree to check
+ * @root_val: the value of the root
+ * Return: 1 if the sub-tree is BST, 0 other-wise
+ */
+int check_left(binary_tree_t *tree, int root_val)
+{
+	binary_tree_t *l_node = NULL, *r_node = NULL;
+
+	if (tree == NULL)
+		return (1);
+
+	l_node = tree->left;
+	r_node = tree->right;
 	/* if the tree is a leaf */
 	if (!l_node && !r_node)
 		return (1);
 	/* if the left tree is null and the right tree value is > tree value */
 	if (!l_node && r_node->n > tree->n)
+	{
+		if (r_node->n > root_val)
+			return (0);
 		return (1);
+	}
 	/* if the left tree value is < tree value and the right tree is null */
 	if(l_node->n < tree->n && !r_node)
 		return (1);
- 	/* if left value is > tree value or right value is < tree value */
+	/* if left val is > tree value or right value is < tree value */
 	if (l_node->n > tree->n || r_node->n < tree->n)
 		return (0);
+	if (r_node->n > root_val)
+		return (0);
 	/* recursivly call for the function to check all the sub-trees */
-	return (binary_tree_is_bst(l_node) * binary_tree_is_bst(r_node));
+	return (check_left(l_node, root_val) * check_left(r_node, root_val));
+}
+
+/**
+ * check_right - check the right sub-tree
+ * @tree: the left sub-tree to check
+ * @root_val: the value of the root
+ * Return: 1 if the sub-tree is BST, 0 other-wise
+ */
+int check_right(binary_tree_t *tree, int root_val)
+{
+	binary_tree_t *l_node = NULL, *r_node = NULL;
+
+	if (tree == NULL)
+		return (1);
+
+	l_node = tree->left;
+	r_node = tree->right;
+
+	/* if the tree is a leaf */
+	if (!l_node && !r_node)
+		return (1);
+
+	/* if the left tree is null and the right tree value is > tree value */
+	if (!l_node && r_node->n > tree->n)
+		return (1);
+
+	/* if the left tree value is < tree value and the right tree is null */
+	if(l_node->n < tree->n && !r_node)
+	{
+		if (l_node->n < root_val)
+			return (0);
+		return (1);
+	}
+
+	/* if left val is > tree value or right value is < tree value */
+	if (l_node->n > tree->n || r_node->n < tree->n)
+		return (0);
+
+	if (l_node->n < root_val)
+		return (0);
+	/* recursivly call for the function to check all the sub-trees */
+	return (check_left(l_node, root_val) * check_left(r_node, root_val));
 }
