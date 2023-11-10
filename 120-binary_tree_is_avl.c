36af96a00d4ce87bc4cd5a15b2e688f6939694e4
mojtabababiker <mojmohammad98@gmail.com>
2023-11-10 03:13:35 +0200

    Edit task 120, change code logic

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -1,125 +1,145 @@
 #include "binary_trees.h"
 
-int check_right(binary_tree_t *tree, int root_val);
-int check_left(binary_tree_t *tree, int root_val);
-int get_height(binary_tree_t *tree);
+int is_binary_search_tree(const binary_tree_t *tree);
+binary_tree_t *get_min(binary_tree_t *tree);
+binary_tree_t *get_max(binary_tree_t *tree);
+size_t get_height(const binary_tree_t *tree);
 
 /**
  * binary_tree_is_avl - check if the tree is an AVL binary tree
- * @tree: pointer to the tree to be checked
- * Retrun: 1 if the tree is AVL, 0 other-wise
+ * @tree: pointer to the root node of the tree
+ * Return: 1 if the tree is an AVL, 0 other-wise
+ * Description:
+ *     AVL binary trees conditions:
+ *          1] it is a Binary Search Tree
+ *          2] the balance factor in it is 1, 0, or -1
+ *          3] all its sub-trees are AVL
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-	int is_bstree = 0, is_balanced = 0;
+	int is_bst = 0;
+	int l_height = 0, r_height = 0, balanced = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	is_bstree = check_left(tree->left, tree->n) * check_right(tree->right,
-								  tree->n);
-	if (!is_bstree)
+	is_bst = is_binary_search_tree(tree);
+	if (!is_bst)
 		return (0);
-	is_balanced = get_height(tree->left) - get_height(tree->right);
 
-	if (is_balanced < -1 || is_balanced > 1)
+	l_height = get_height(tree->left);
+	r_height = get_height(tree->right);
+
+	balanced = l_height - r_height;
+	if (balanced > 1 || balanced < -1)
 		return (0);
+
 	return (1);
 }
 
 /**
- * check_left - check the left sub-tree
- * @tree: the left sub-tree to check
- * @root_val: the value of the root
- * Return: 1 if the sub-tree is BST, 0 other-wise
+ * is_binary_search_tree - check if the binary tree given is Binary Search Tree
+ * @tree: pointer to the root node of the tree
+ * Return: 1 if the tree is BST, 0 other-wise
  */
-int check_left(binary_tree_t *tree, int root_val)
+int is_binary_search_tree(const binary_tree_t *tree)
 {
-	binary_tree_t *l_node = NULL, *r_node = NULL;
+	int is_bst = 0;
+	binary_tree_t *max_left = NULL, *min_right = NULL;
 
 	if (tree == NULL)
 		return (1);
 
-	l_node = tree->left;
-	r_node = tree->right;
-	/* if the tree is a leaf */
-	if (!l_node && !r_node)
+	if (!tree->left && !tree->right)
 		return (1);
-	/* if the left tree is null and the right tree value is > tree value */
-	if (!l_node && r_node->n > tree->n)
-	{
-		if (r_node->n > root_val)
-			return (0);
-		return (1);
-	}
-	/* if the left tree value is < tree value and the right tree is null */
-	if (l_node->n < tree->n && !r_node)
-		return (1);
-	/* if left val is > tree value or right value is < tree value */
-	if (l_node->n > tree->n || r_node->n < tree->n)
+	if (tree->left)
+		max_left = get_max(tree->left);
+	if (tree->right)
+		min_right = get_min(tree->right);
+
+	is_bst = is_binary_search_tree(tree->left) *
+		is_binary_search_tree(tree->right);
+
+	/* the root value of the subtree should be less than all it's */
+	/* + right siplings values, and greater than all it's left */
+	/* + siplings values, if NOT then return zero */
+	if (max_left && tree->n <= max_left->n)
 		return (0);
-	if (r_node->n > root_val)
+	if (min_right && tree->n >= min_right->n)
 		return (0);
-	/* recursivly call for the function to check all the sub-trees */
-	return (check_left(l_node, root_val) * check_left(r_node, root_val));
+
+	return (is_bst);
 }
 
 /**
- * check_right - check the right sub-tree
- * @tree: the left sub-tree to check
- * @root_val: the value of the root
- * Return: 1 if the sub-tree is BST, 0 other-wise
+ * get_min - find the minimum value in the sub-tree
+ * @tree: the root of the sub-tree to check
+ * Return: pointer to the node witth the minimum value, or NULL in failure
  */
-int check_right(binary_tree_t *tree, int root_val)
+binary_tree_t *get_min(binary_tree_t *tree)
 {
-	binary_tree_t *l_node = NULL, *r_node = NULL;
+	binary_tree_t *min_left = NULL, *min_right = NULL, *min = NULL;
 
 	if (tree == NULL)
-		return (1);
+		return (NULL);
 
-	l_node = tree->left;
-	r_node = tree->right;
+	if (!tree->left && !tree->right)
+		return (tree);
 
-	/* if the tree is a leaf */
-	if (!l_node && !r_node)
-		return (1);
+	min = tree;
+	min_left = get_min(tree->left);
+	min_right = get_min(tree->right);
 
-	/* if the left tree is null and the right tree value is > tree value */
-	if (!l_node && r_node->n > tree->n)
-		return (1);
-
-	/* if the left tree value is < tree value and the right tree is null */
-	if (l_node->n < tree->n && !r_node)
-	{
-		if (l_node->n < root_val)
-			return (0);
-		return (1);
-	}
-
-	/* if left val is > tree value or right value is < tree value */
-	if (l_node->n > tree->n || r_node->n < tree->n)
-		return (0);
-	if (l_node->n < root_val)
-		return (0);
-	/* recursivly call for the function to check all the sub-trees */
-	return (check_right(l_node, root_val) * check_right(r_node, root_val));
+	if (min_left && min_left->n < min->n)
+		min = min_left;
+	if (min_right && min_right->n < min->n)
+		min = min_right;
+	return (min);
 }
 
 /**
- * get_height - calcualte the height of a tree
- * @tree: a binary tree to calculate its height
- * Return: the heihgt of the tree
+ * get_max - find the maximum value in the sub-tree
+ * @tree: the root of the sub-tree to check
+ * Return: pointer to the node witth the maximum value, or NULL in failure
  */
-int get_height(binary_tree_t *tree)
+binary_tree_t *get_max(binary_tree_t *tree)
 {
-	int l_height = 0, r_height = 0;
+	binary_tree_t *max_left = NULL, *max_right = NULL, *max = NULL;
+
+	if (tree == NULL)
+		return (NULL);
+
+	if (!tree->left && !tree->right)
+		return (tree);
+
+	max = tree;
+	max_left = get_max(tree->left);
+	max_right = get_max(tree->right);
+
+	if (max_left && max_left->n > max->n)
+		max = max_left;
+	if (max_right && max_right->n > max->n)
+		max = max_right;
+	return (max);
+}
+
+/**
+ * get_height - recursivly calculate the height of tree
+ * @tree: the root node of the tree
+ * Return: the height of the tree
+ */
+size_t get_height(const binary_tree_t *tree)
+{
+	size_t l_height = 0, r_height = 0;
 
 	if (tree == NULL)
 		return (0);
+
 	l_height += get_height(tree->left);
 	r_height += get_height(tree->right);
 
 	if (l_height > r_height)
 		return (++l_height);
+
 	return (++r_height);
 }
